91367ae7f4a37b84f721a308b779bf0eaa41ab50
ゆうさと００ <sathuu.game@gmail.com>
2023-03-02 19:25:55 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class Book extends TangibleAsset{
+	private String isbn;
+	public Book(String name,int price,string color,String isbn){
+		super(name,price,color);
+		this.isbn = isbn;
+	}
+	public String getIsbn(){rturn this.isbn;}
+
+}
